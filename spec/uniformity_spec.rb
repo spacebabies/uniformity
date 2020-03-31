bd5c2a5c1cc6e683c9ec93d7604706fbf4eaca62
Joost Baaij <joost@spacebabies.nl>
2020-03-31 10:29:07 +0200

    mitigate CVE-2020-8130

--- spec/uniformity_spec.rb
+++ spec/uniformity_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Uniformity do
   it "has a version number" do
     expect(Uniformity::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
